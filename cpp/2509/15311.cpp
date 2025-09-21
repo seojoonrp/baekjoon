caaded12f972ee0680199db664b1921d8d4c917f
seojoonrp <seojoonrp@snu.ac.kr>
2025-09-21 22:36:03 +0900

    250921: 15311 Random constructive problem

---
+++ cpp/2509/15311.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  printf("2000\n");
+  for (int i = 1; i <= 1000; i++) {
+    printf("1 ");
+  }
+  for (int i = 1; i <= 1000; i++) {
+    printf("1000 ");
+  }
+  printf("\n");
+}
